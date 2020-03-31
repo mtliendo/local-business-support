432868e350549c902e7dd871557d6e6b7599efc1
Michael Liendo <mtliendo@Michaels-MacBook-Pro.local>
2020-03-31 03:17:50 -0500

    uncomment import

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import Helmet from "react-helmet"
 import PropTypes from "prop-types"
 import { Header } from "components"
 import { Layout, Container } from "layouts"
-// import { NavBar } from "../layouts"
+import { NavBar } from "../layouts"
 
 const About = center => (
   <Layout>
