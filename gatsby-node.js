5634e282bb0d29a5bf99ebcdfe09281261a137ed
Michael Liendo <mtliendo@Michaels-MacBook-Pro.local>
2020-03-31 02:40:27 -0500

    remove auth and v1 components

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,18 @@
 const path = require("path")
 
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = `/app/*`
+
+    // Update the page.
+    createPage(page)
+  }
+}
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
